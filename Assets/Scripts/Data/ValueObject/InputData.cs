d0e2e4801a2685dc8863c2919695d01b2783bac2
EnesTalhaYilmaz <ensylmz1441@gmail.com>
2022-07-19 13:44:47 +0300

    Input Manager Completed

---
+++ Assets/Scripts/Data/ValueObject/InputData.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace Data.ValueObject
+{
+    public class InputData
+    {
+        public float HorizontalInputSpeed =2f;
+        public Vector2 ClampSides = new Vector2(-3, 3);
+        public float ClampSpeed = 0.007f;
+    }
+}
